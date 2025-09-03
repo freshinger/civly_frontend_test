3ffb4a5f5b4fe5048d8b60752ba2e9ee73b25d4d
Daniel <daniel_ciecior@yahoo.de>
2025-09-03 10:26:12 +0200

    signout action implemented

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -71,7 +71,6 @@ export async function updateSession(request: NextRequest) {
   if (
     !user &&
     !request.nextUrl.pathname.startsWith('/login') &&
-    !request.nextUrl.pathname.startsWith('/auth') &&
     !request.nextUrl.pathname.startsWith('/error')
   ) {
     // no user, potentially respond by redirecting the user to the login page
